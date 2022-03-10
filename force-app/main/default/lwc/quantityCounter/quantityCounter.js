084d74ff5cfbaba8fd87f52fd84946ed2f7121c2
amata7 <adrianmata@live.com>
2022-03-10 12:10:22 -0800

    submit button will change quantity to 0

--- force-app/main/default/lwc/quantityCounter/quantityCounter.js
+++ force-app/main/default/lwc/quantityCounter/quantityCounter.js
@@ -3,6 +3,8 @@ import { LightningElement, api } from 'lwc';
 export default class QuantityCounter extends LightningElement {
   @api counterVal = 0;
   @api incrementVal = 1;
+  @api itemImgUrl;
+  @api itemTitle;
 
   handleDecrement(e) {
     if (this.counterVal === 0) {
@@ -14,5 +16,9 @@ export default class QuantityCounter extends LightningElement {
 
   handleIncrement(e) {
     this.counterVal = parseInt(this.counterVal) + parseInt(this.incrementVal);
+  }
+
+  handleClick(e) {
+    this.counterVal = 0;
   }
 }
