f47d420a83f24a36077912ea988d153a03c631f5
amata7 <adrianmata@live.com>
2022-03-10 10:40:31 -0800

    removed quantity preview and quantity will not go lower than 0 when decrementing

--- force-app/main/default/lwc/quantityCounter/quantityCounter.js
+++ force-app/main/default/lwc/quantityCounter/quantityCounter.js
@@ -5,16 +5,14 @@ export default class QuantityCounter extends LightningElement {
   @api incrementVal = 1;
 
   handleDecrement(e) {
-    this.counterVal = parseInt(this.counterVal) - parseInt(this.incrementVal);
-    this.dispatchEvent(
-      new CustomEvent('counterchange', { detail: this.counterVal })
-    );
+    if (this.counterVal === 0) {
+      this.counterVal = parseInt(this.counterVal);
+    } else {
+      this.counterVal = parseInt(this.counterVal) - parseInt(this.incrementVal);
+    }
   }
 
   handleIncrement(e) {
     this.counterVal = parseInt(this.counterVal) + parseInt(this.incrementVal);
-    this.dispatchEvent(
-      new CustomEvent('counterchange', { detail: this.counterVal })
-    );
   }
 }
--- force-app/main/default/lwc/quantityCounterPreview/quantityCounterPreview.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-export default class QuantityCounterPreview extends LightningElement {
-  @track counterVal = 0;
-  handleCounterChange(e) {
-    this.counterVal = e.detail;
-  }
-}
